52270f7d6ef762800484748eac48c92cba798a7e
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2019-07-11 22:23:06 +0200

    Update

---
+++ Xamarin.Droid.Hdodenhof.CircleImageView/Additions/CircleImageView.cs
@@ -0,0 +1,15 @@
+﻿namespace De.Hdodenhof.CircleImageView
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Android.Content;
+    using Android.Content.Res;
+    using Android.Util;
+
+    public partial class CircleImageView
+    {
+    }
+}
